c5640155c15b830cfda260921b15e1b970ada93f
Marat <maratayup@yandex.ru>
2024-12-26 21:12:30 +0300

    final front news

---
+++ PcClub_Api/target/classes/static/scripts/components/header-component.js
@@ -0,0 +1,137 @@
+let header = document.getElementById("header");
+
+const url = "";
+
+const getUserRole = async () => {
+  const response = await fetch(url);
+  const result = await response.json();
+};
+
+const currentUser = "admin";
+
+const urlForLogo = "images/icons/icon-club.png";
+const urlForAccountPage = "account-page.html";
+const urlForPricePage = "price-page.html";
+const urlForUserTablePage = "user-table-page.html";
+const urlLogoForIndex = "./";
+const urlLogoForPages = ".././";
+const urlPagesForIndex = "./pages/";
+const urlPagesForPages = "./";
+
+const htmlForAdmin = `<li class="header_dropdonw-menu-item-area">
+                      <div class="header_dropdown-menu-item-shadow">
+                        <a
+                          class="header_dropdonw-menu-item"
+                          href=${
+                            document.URL.includes("index")
+                              ? urlPagesForIndex + urlForUserTablePage
+                              : urlPagesForPages + urlForUserTablePage
+                          }
+                          >КЛИЕНТЫ</a
+                        >
+                      </div>
+                    </li>
+                   `;
+
+header.insertAdjacentHTML(
+  "beforeend",
+  `<header class="header_area">
+        <div class="header_container">
+          <div class="header_logo-area">
+            <div class="header_icon-logo-shadow">
+              <img
+                src=${
+                  document.URL.includes("index")
+                    ? urlLogoForIndex + urlForLogo
+                    : urlLogoForPages + urlForLogo
+                }
+                class="header_icon-logo"
+              />
+            </div>
+            <div class="header_name-club-area">
+              <div class="header_name-club">IT GAMER CLUB</div>
+            </div>
+          </div>
+          <div>
+            <nav>
+              <ul class="header_menu-area-list">
+                <li id="home-page" class="header_menu-item-area">
+                  <a class="header_menu-item" href=${
+                    document.URL.includes("index") ? "" : "../index.html"
+                  }
+                    >ГЛАВНАЯ</a
+                  >
+                </li>
+                <li id="btn-animate" class="header_menu-item-area">
+                  <button
+                    id="button"
+                    class="header_menu-item header_remove-button"
+                  >
+                    МЕНЮ
+                  </button>
+                </li>
+                <div id="header-menu" class="header_dropdown-menu">
+                  <ul class="header_dropdown-menu-list">
+                    <li class="header_dropdonw-menu-item-area">
+                      <div class="header_dropdown-menu-item-shadow">
+                        <a
+                          class="header_dropdonw-menu-item"
+                          href=${
+                            document.URL.includes("index")
+                              ? urlPagesForIndex + urlForAccountPage
+                              : urlPagesForPages + urlForAccountPage
+                          }
+                          >АККАУНТ</a
+                        >
+                      </div>
+                    </li>
+                    ${currentUser == "admin" ? htmlForAdmin : ``}
+                    <li class="header_dropdonw-menu-item-area">
+                      <div class="header_dropdown-menu-item-shadow">
+                        <a
+                          class="header_dropdonw-menu-item"
+                          href=${
+                            document.URL.includes("index")
+                              ? urlPagesForIndex + urlForPricePage
+                              : urlPagesForPages + urlForPricePage
+                          }
+                          >ЦЕНЫ</a
+                        >
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>`
+);
+
+const buttonMenu = document.getElementById("button");
+const headerMenu = document.getElementById("header-menu");
+const btnAnimate = document.getElementById("btn-animate");
+
+document.addEventListener("DOMContentLoaded", () => {
+  buttonMenu.addEventListener("click", () => {
+    if (headerMenu.classList.contains("header_open")) {
+      if (currentUser == "admin") {
+        btnAnimate.classList.remove("header_btn-open-admin");
+      } else if (currentUser == "moderator") {
+        btnAnimate.classList.remove("header_btn-open-moderator");
+      } else {
+        btnAnimate.classList.remove("header_btn-open");
+      }
+      headerMenu.classList.remove("header_open");
+    } else {
+      headerMenu.classList.add("header_open");
+      if (currentUser == "admin") {
+        btnAnimate.classList.add("header_btn-open-admin");
+      } else if (currentUser == "moderator") {
+        btnAnimate.classList.add("header_btn-open-moderator");
+      } else {
+        btnAnimate.classList.add("header_btn-open");
+      }
+    }
+  });
+});
--- PcClub_Api/target/classes/static/scripts/home-page/home-page.js
+++ PcClub_Api/target/classes/static/scripts/home-page/home-page.js
@@ -33,7 +33,7 @@ const newsArea = document.getElementById("news-area");
 
 const clickRedactIcon = (button) => {
   button.classList.add("none");
-  button.parentNode.firstChild.classList.remove("none");
+  button.parentNode.children[0].classList.remove("none");
 
   let news = button.parentNode.parentNode;
 
@@ -52,7 +52,6 @@ const clickConfirmIcon = (button) => {
 
   let header = news.children[2];
   let body = news.children[3];
-  console.log(body.firstChild.textContent);
   header.innerHTML = `${header.firstChild.value}`;
   body.innerHTML = `${body.firstChild.value}`;
 };
@@ -62,58 +61,89 @@ const clickTrashIcon = (button) => {
   div.parentNode.removeChild(div);
 };
 
+const clickPlusIcon = (button) => {
+  let div = button.parentNode;
+  addNewNews("Заголовок", "Содержание", "");
+  div.parentNode.removeChild(div);
+  createPlusIcon();
+};
+
+const createPlusIcon = () => {
+  let newNews = document.createElement("div");
+  newNews.classList.add("news");
+  newNews.classList.add("flex");
+  newNews.classList.add("news-animation");
+
+  let iconPlus = document.createElement("img");
+  iconPlus.classList.add("icon-plus");
+  iconPlus.src = "./images/icons/icon-plus.png";
+  iconPlus.addEventListener("click", () => {
+    clickPlusIcon(iconPlus);
+  });
+
+  newNews.appendChild(iconPlus);
+  newsArea.appendChild(newNews);
+};
+
+const addNewNews = (headerContent, bodyContent, imgContent) => {
+  const newsHeader = document.createElement("div");
+  newsHeader.classList.add("news-header");
+  newsHeader.innerHTML = headerContent;
+
+  const newsImg = document.createElement("img");
+  newsImg.classList.add("news-img");
+  newsImg.src = imgContent;
+
+  const newsBody = document.createElement("div");
+  newsBody.classList.add("news-body");
+  newsBody.innerHTML = bodyContent;
+
+  const news = document.createElement("div");
+  news.classList.add("news");
+  news.classList.add("news-animation");
+  news.appendChild(newsImg);
+  if (userRole == "admin" || userRole == "moderator") {
+    const redactElements = document.createElement("div");
+    redactElements.classList.add("redact-area");
+
+    const redactIcon = document.createElement("img");
+    redactIcon.classList.add("redact-icon");
+    redactIcon.src = "./images/icons/icon-change.png";
+    redactIcon.addEventListener("click", () => {
+      clickRedactIcon(redactIcon);
+    });
+
+    const confirmIcon = document.createElement("img");
+    confirmIcon.classList.add("redact-icon");
+    confirmIcon.classList.add("none");
+    confirmIcon.src = "./images/icons/icon-confirm.png";
+    confirmIcon.addEventListener("click", () => {
+      clickConfirmIcon(confirmIcon);
+    });
+
+    const trashIcon = document.createElement("img");
+    trashIcon.src = "./images/icons/icon-trash.png";
+    trashIcon.classList.add("redact-icon");
+    trashIcon.addEventListener("click", () => {
+      clickTrashIcon(trashIcon);
+    });
+
+    redactElements.appendChild(confirmIcon);
+    redactElements.appendChild(redactIcon);
+    redactElements.appendChild(trashIcon);
+    news.appendChild(redactElements);
+  }
+  news.appendChild(newsHeader);
+  news.appendChild(newsBody);
+  newsArea.appendChild(news);
+};
+
 const addNews = () => {
   for (let i = 0; i < newsArray.length; i++) {
-    const newsHeader = document.createElement("div");
-    newsHeader.classList.add("news-header");
-    newsHeader.innerHTML = newsArray[i].header;
-
-    const newsImg = document.createElement("img");
-    newsImg.classList.add("news-img");
-    newsImg.src = newsArray[i].img;
-
-    const newsBody = document.createElement("div");
-    newsBody.classList.add("news-body");
-    newsBody.innerHTML = newsArray[i].body;
-
-    const news = document.createElement("div");
-    news.classList.add("news");
-    news.classList.add("news-animation");
-    news.appendChild(newsImg);
-    if (userRole == "admin" || userRole == "moderator") {
-      const redactElements = document.createElement("div");
-      redactElements.classList.add("redact-area");
-
-      const redactIcon = document.createElement("img");
-      redactIcon.classList.add("redact-icon");
-      redactIcon.src = "./images/icons/icon-change.png";
-      redactIcon.addEventListener("click", () => {
-        clickRedactIcon(redactIcon);
-      });
-
-      const confirmIcon = document.createElement("img");
-      confirmIcon.classList.add("redact-icon");
-      confirmIcon.classList.add("none");
-      confirmIcon.src = "./images/icons/icon-confirm.png";
-      confirmIcon.addEventListener("click", () => {
-        clickConfirmIcon(confirmIcon);
-      });
-
-      const trashIcon = document.createElement("img");
-      trashIcon.src = "./images/icons/icon-trash.png";
-      trashIcon.classList.add("redact-icon");
-      trashIcon.addEventListener("click", () => {
-        clickTrashIcon(trashIcon);
-      });
-
-      redactElements.appendChild(confirmIcon);
-      redactElements.appendChild(redactIcon);
-      redactElements.appendChild(trashIcon);
-      news.appendChild(redactElements);
-    }
-    news.appendChild(newsHeader);
-    news.appendChild(newsBody);
-    newsArea.appendChild(news);
+    addNewNews(newsArray[i].header, newsArray[i].body, newsArray[i].img);
+  }
+  if (userRole == "moderator" || userRole == "admin") {
+    createPlusIcon();
   }
 };
 
