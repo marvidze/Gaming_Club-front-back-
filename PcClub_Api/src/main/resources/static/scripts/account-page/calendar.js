38dbe5356fe9bbdca1d8426feaa09bdb08e2263e
Marat <maratayup@yandex.ru>
2024-12-28 01:25:17 +0300

    сделал функцию renderTimes

--- PcClub_Api/src/main/resources/static/scripts/account-page/calendar.js
+++ PcClub_Api/src/main/resources/static/scripts/account-page/calendar.js
@@ -22,13 +22,69 @@ arraySevenDays.forEach((item, index) => {
   item.innerText = formattedDate;
 });
 
+const renderTimes = (element) => {
+  let arrayAllDates = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+  ];
+
+  let date = new Date();
+  const day = String(date.getDate()).padStart(2, "0");
+  let idForCheckbox = 0;
+  let plusNumber = element.htmlFor;
+
+  let arrayDates = new Array();
+
+  for (let i = 0; i < arrayDates.length; i++) {
+    if (arrayDates[i].date == Number(day) + Number(plusNumber)) {
+      let index = arrayAllDates.indexOf(arrayDates[i].time);
+      arrayAllDates.splice(index, 1);
+    }
+  }
+
+  let timesRow = document.getElementById("times-row");
+  let checkboxes = ``;
+
+  for (let i = 0; i < arrayAllDates.length; i++) {
+    let checkbox = `
+    <input
+      class="input_time"
+      type="checkbox"
+      id=${"time" + idForCheckbox}
+      hidden
+    />
+    <label class="label_time" for=${"time" + idForCheckbox}>
+      ${
+        arrayAllDates[i] < 10
+          ? "0" + arrayAllDates[i] + " : 00"
+          : arrayAllDates[i] + " : 00"
+      }
+    </label>
+    `;
+
+    idForCheckbox++;
+
+    checkboxes += checkbox;
+  }
+
+  timesRow.innerHTML = checkboxes;
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
   if (
     localStorage.getItem("login") != null &&
     localStorage.getItem("password") != null
   ) {
-    let idForCheckbox = 0;
-
     const selectedZone = document.querySelector("#form_zones input:checked");
     let date = new Date();
     const year = date.getFullYear().toString();
@@ -36,63 +92,18 @@ document.addEventListener("DOMContentLoaded", async () => {
     const day = String(date.getDate()).padStart(2, "0");
     const resultDate = `${year}.${month}.${day}`;
 
-    console.log(urlGetTimes + selectedZone.id);
     const response = await fetch(urlGetTimes + selectedZone.id);
     const result = await response.json();
 
-    let arrayDates = result; // массив объектов
-
-    arrayDates = [
-      {
-        date: "27",
-        time: "1",
-      },
-      {
-        date: "27",
-        time: "2",
-      },
-      {
-        date: "27",
-        time: "3",
-      },
-      {
-        date: "27",
-        time: "4",
-      },
-    ];
-
-    let timesRow = document.getElementById("times-row");
-    let checkboxes = ``;
-
-    for (let i = 0; i < arrayDates.length; i++) {
-      let checkbox = `
-      <input
-        class="input_time"
-        type="checkbox"
-        id=${"time" + idForCheckbox}
-        hidden
-      />
-      <label class="label_time" for=${"time" + idForCheckbox}>
-        ${
-          arrayDates[i].time < 10
-            ? "0" + arrayDates[i].time + " : 00"
-            : arrayDates[i].time + " : 00"
-        }
-      </label>
-      `;
-
-      idForCheckbox++;
-
-      checkboxes += checkbox;
-    }
-
-    timesRow.innerHTML = checkboxes;
+    let arrayDates = result;
+    const selectedDay = document.querySelector("#form_dates input:checked");
   }
 });
 
 btnReservation.addEventListener("click", async () => {
   const selectedZone = document.querySelector("#form_zones input:checked");
   const selectedDay = document.querySelector("#form_dates input:checked");
+  console.log(selectedDay);
   const arraySelectedTimes = document.querySelectorAll(
     "#form_times input:checked"
   );
